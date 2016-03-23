37083c7b8808b75bc25fe6bf63b737c3c8c9a66a
Malcolm Davis Steele <malcolm.dast@gmail.com>
2016-03-22 21:10:24 -0600

    Led funtion and check button help functions added.

--- Program/sketch_mar20a/sketch_mar20a.ino
+++ Program/sketch_mar20a/sketch_mar20a.ino
@@ -1,10 +1,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
+#define delayTime 250
+
 void ledFuntion(short *);
 bool LEDMODE=0;
 bool BUTTONSTATUS=0;
-int rep=3;
+int rep=1;
 
 int main()
 {
@@ -14,6 +16,7 @@ int main()
   short* Masks[2]= {Mask1, Mask2};
   int i=0;
   while(1){
+    checkButton();
     if(LEDMODE){
       for(short i=0; i<rep; i++){
         ledFuntion(*(Masks+1));
@@ -21,21 +24,24 @@ int main()
       LEDMODE=0;
     }
     ledFuntion(*Masks);
-    if(PINB && !BUTTONSTATUS){
-      LEDMODE=1;
-      BUTTONSTATUS=1;  
-    }
-    if(!PINB && BUTTONSTATUS){
-      BUTTONSTATUS=0;
-    }
   }
 }
 
 void ledFuntion(short * mask){
-  int delayTime=300;
   for(int i=0; i<3; i++){
     PORTB = *(mask+i);
     _delay_ms(delayTime);
+    checkButton();
   }
+}
+
+void checkButton(){
+    if(PINB==1 && !BUTTONSTATUS){
+      LEDMODE=1;
+      BUTTONSTATUS=1;  
+    }
+    if(PINB==0 && BUTTONSTATUS){
+      BUTTONSTATUS=0;
+    }
 }
 
